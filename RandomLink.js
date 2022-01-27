cf1d7ef1c587d04c145fc120db873038822fba68
Blueishere <84488513+hoangtuan30124@users.noreply.github.com>
2022-01-27 10:30:32 +0700

    Add files via upload

---
+++ RandomLink.js
@@ -0,0 +1,22 @@
+var sites = [
+    'http://reddit.com',
+    'http://www.google.com',
+    'http://www.stackoverflow.com',
+    'http://www.example.com',
+    'http://www.youtube.com',
+    'http://facebook.com'
+];
+
+function randomSite() {
+    var i = Math.floor(Math.random()*5000)+1
+    if(i == 1410){
+        var index = 0
+    }
+    else if(i<=4500){
+        var index = 1
+    }
+    else if(i <=5000){
+        var index = Math.floor(Math.random()*4)+2
+    }
+    location.href = sites[index];
+}
